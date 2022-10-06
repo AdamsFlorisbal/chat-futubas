adecda8d0ecb887d1aec6ffabdebd40e80f380f3
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-10-06 00:20:31 -0300

    Create/cjhat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Chat from "./components/Chat";
+import Sidebar from "./components/Sidebar";
+import * as C from "./styles/app";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "./services/firebase";
-import Login from "./components/Login/index";
-import Loading from "./components/Loading/Index";
-import Sidebar from "./components/Sidebar/index";
+import Login from "./components/Login";
+import Loading from "./components/Loading";
 
 const App = () => {
   const [user, loading] = useAuthState(auth);
+  const [userChat, setUserChat] = useState(null);
 
   useEffect(() => {
     if (user) {
@@ -22,10 +25,11 @@ const App = () => {
   if (!user) return <Login />;
 
   return (
-    <>
-    <Sidebar/>
-    </>
-  )
-}
+    <C.Container>
+      <Sidebar setUserChat={setUserChat} userChat={userChat} />
+      <Chat userChat={userChat} />
+    </C.Container>
+  );
+};
 
-  export default App;
+export default App;
